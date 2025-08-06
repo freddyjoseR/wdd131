7c747873174daea03871b76bb3b91ab4e4b598ae
freddyjoseR <freddy.rodriguez.saa@gmail.com>
2025-08-06 00:30:00 -0600

    siteplan document

---
+++ project/scripts/siteplan.js
@@ -0,0 +1,6 @@
+// Current date
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+// Modification date
+document.getElementById("lastModified").textContent = document.lastModified;
